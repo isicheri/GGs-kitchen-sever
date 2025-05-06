0e225306bde0d93236fdd0604a8ead384d89637a
isicher <tech2dom@gmail.com>
2025-05-06 15:33:31 +0100

    made another change

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,24 @@ import { port } from "./utils/secrets";
 import errorMiddleware from "./middleware/error.middleware";
 import indexRouter from "./routes";
 import loggerMiddleware from "./middleware/logger.middleware";
+import { EVIRONMENT } from "./utils/secrets";
 
 const App:Express = express();
 App.use(express.json());
-App.use(cors());
+App.use(cors({origin: "*",methods: ["GET","POST","PUT","DELETE"]}));
 App.use(helmet());
 App.use(loggerMiddleware);
 App.use('/api/v1',indexRouter);
-
-
-
-
 App.use(errorMiddleware);
-App.listen(process.env.PORT,() => {
-    console.log(`server is running on port ${port}`)
-});
+
+console.log(EVIRONMENT)
+
+if(EVIRONMENT === "production") {
+    console.log("In prod mode")
+  App.listen();  
+}else {
+    console.log("In dev mode")
+    App.listen(process.env.PORT,() => {
+        console.log(`server is running on port ${port}`)
+    });
+}
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -2,4 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const port =  process.env.PORT;
-export const JWT_SECRET = process.env.JWT_SEC
+export const JWT_SECRET = process.env.JWT_SEC;
+export const EVIRONMENT = process.env.NODE_ENVIRONMENT;
