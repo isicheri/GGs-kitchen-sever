7c957450ef4080ed1129786e57ce469ae9f3d71a
isicher <tech2dom@gmail.com>
2025-06-20 23:47:51 +0100

    another one

--- dist/middleware/logger.middleware.js
+++ dist/middleware/logger.middleware.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const chalk_1 = __importDefault(require("chalk"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const loggerMiddleware = (req, res, next) => {
     const method = req.method;
     const url = req.originalUrl;
+    const host = req.host;
     const timeStamp = new Date().toISOString();
-    console.log(`${chalk_1.default.green(`request-time: ${timeStamp}`)}${chalk_1.default.yellow(`request-method: ${method}`)} ${chalk_1.default.red(`request-url:localhost:4000/${url}`)}`);
+    console.log(`${chalk_1.default.green(`request-time: ${timeStamp}`)} ${chalk_1.default.yellow(`request-method: ${method}`)} ${chalk_1.default.red(`request-url: ${host}${url}`)}`);
     next();
 };
 exports.default = loggerMiddleware;
--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,10 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config()
 const loggerMiddleware = (req:Request,res:Response,next:NextFunction) => {
     const method = req.method;
     const url = req.originalUrl;
+    const host = req.host;
     const timeStamp = new Date().toISOString();
-    console.log(`${chalk.green(`request-time: ${timeStamp}`)}${chalk.yellow(`request-method: ${method}`)} ${chalk.red(`request-url:localhost:4000/${url}`)}`)
+    console.log(`${chalk.green(`request-time: ${timeStamp}`)} ${chalk.yellow(`request-method: ${method}`)} ${chalk.red(`request-url: ${host}${url}`)}`)
    next()
 }
 export default loggerMiddleware;
